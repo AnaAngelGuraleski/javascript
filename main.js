dbd9bb5ca9d0d3bb4a9a048f2be4dbfa12a54578
Ana Angel <141441898+AnaAngelGuraleski@users.noreply.github.com>
2024-09-25 08:06:58 -0300

    g

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function soma(num1,num2) {
     return  num1+num2;
 }
 console.log(soma(7,8));
-function calcule(num3,num3) {
+function calcule(num3) {
     return  num3*num3;
 }
+console.log(calcule(7))

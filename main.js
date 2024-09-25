7be8538b6574ce229e2ea36e51174f232f8584cf
Ana Angel <141441898+AnaAngelGuraleski@users.noreply.github.com>
2024-09-25 08:15:21 -0300

    .

--- main.js
+++ main.js
@@ -5,4 +5,8 @@ console.log(soma(7,8));
 function areaQuadrado(lado) {
     return  lado*lado;
 }
-console.log(areaQuadrado(7))
+console.log(areaQuadrado(7));
+let media = function(num4,num5,num6 ){
+    return  num4+num5+num6/3;
+}
+console.log(media);

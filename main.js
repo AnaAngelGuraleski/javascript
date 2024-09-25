8d40eca7c14284530d9a3baa60f01c0f4c77219e
Ana Angel <141441898+AnaAngelGuraleski@users.noreply.github.com>
2024-09-25 08:09:54 -0300

    .

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function soma(num1,num2) {
     return  num1+num2;
 }
 console.log(soma(7,8));
-function areaQuadrado(num3) {
-    return  num3*num3;
+function areaQuadrado(lado) {
+    return  lado*lado;
 }
 console.log(areaQuadrado(7))
